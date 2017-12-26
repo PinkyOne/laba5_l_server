5c7798cc44a2cd30da01c1e7e37c90bb5efea495
Анастасия Орлова <orlovaad@gmail.com>
2017-12-26 19:08:16 +0400

    Начато добавление тестов

--- src/main/java/com/bank/teller/util/DBUtil.java
+++ src/main/java/com/bank/teller/util/DBUtil.java
@@ -11,7 +11,9 @@ public class DBUtil {
     private static EmbeddedDatabase getDataSource() {
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
 
-        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).addScript("sql/createschema.sql").addScript("sql/insertdata.sql").build();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY)
+                .addScript("sql/createschema.sql")
+                .addScript("sql/insertdata.sql").build();
         return db;
     }
 
